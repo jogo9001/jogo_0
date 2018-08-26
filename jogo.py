50951d1ed643aa9c63650523e2893819b78000bd
asadefrango <andre.santana.de.araujo@gmail.com>
2018-08-26 00:04:24 -0400

    sair

--- jogo.py
+++ jogo.py
@@ -3,7 +3,7 @@
 import os
 def inicio():
     clear()
-    print(os.name)
+    print("System: ",os.name)
 
 def clear():
     if os.name == "posix":
@@ -20,12 +20,17 @@ def start():
 def config():
     print("aqui configura tudo")
 
+def sair():
+    print("saindo...")
+
 def tela_inicial():
-    opc=input("1-Iniciar\n2-Opções\n: ")
+    opc=input("1-Iniciar\n2-Opções\n\n0-Sair\n\n\n: ")
     if opc == "1":
         start()
     elif opc == "2":
         config()
+    elif opc == "0":
+        sair()
     else:
         clear()
         tela_inicial()
