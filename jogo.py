8923d805e90a26586073ef28b3c7098b97453a94
asadefrango <andre.santana.de.araujo@gmail.com>
2018-08-25 22:15:15 -0400

    novas func

--- jogo.py
+++ jogo.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import os
 def inicio():
     if os.name == "posix":
@@ -10,6 +11,33 @@ def inicio():
     else:
         print('Não estou num unix nem num win')
 
+def clear():
+    if os.name == "posix":
+        os.system('clear')
+    elif os.name in ("nt", "dos", "ce"):
+        os.system('cls')
 
 
+def start():
+    print("aqui comeca o jogo")
+
+def config():
+    print("aqui configura tudo")
+
+def tela_inicial():
+    opc=input("1-Iniciar\n2-Opções\n: ")
+    if opc == "1":
+        start()
+    elif opc == "2":
+        config()
+    else:
+        clear()
+        tela_inicial()
+
+
+
+if __name__ == "__main__":
+
+    inicio()
+    tela_inicial()
 
