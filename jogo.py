cd0fc6327ead49a0ef47e61d3b0a086f233eb822
asadefrango <andre.santana.de.araujo@gmail.com>
2018-08-26 01:04:17 -0400

    config update

--- jogo.py
+++ jogo.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
+
+n_cidades = 10
+prob_ataque = 0.01
+max_dist = 100
+
 def inicio():
     clear()
     print("System: ",os.name)
@@ -12,28 +17,56 @@ def clear():
         os.system('cls')
     else:
         print('Não estou num win nem num unix')
+        sair()
 
+def start(n_c, p_a, m_d):
+    print("aqui comeca o jogo",n_c,p_a,m_d)
 
-def start():
-    print("aqui comeca o jogo")
+def config(n_cidades,prob_ataque,max_dist):
+    print("configurações:")
+    tmp=input("numero de cidades (Atual "+str(n_cidades)+") :"  )
+    try:    
+        int(tmp)
+        n_cidades = tmp
+        print("novo valor:",n_cidades)
+    except:
+        pass
 
-def config():
-    print("aqui configura tudo")
+    tmp=input("probabilidade de ataques (Atual "+str(prob_ataque)+") :"  )
+    try: 
+        float(prob_ataque)
+        prob_ataque = tmp
+        print("novo valor",prob_ataque)
+    except:
+        pass
+
+    tmp=input("distancia maxima entre cidades (Atual "+str(max_dist)+") :"  )
+    try:
+        float(max_dist)
+        max_dist = tmp
+        print("novo valor", max_dist)
+    except:
+        pass
+    return n_cidades,prob_ataque,max_dist
 
 def sair():
     print("saindo...")
+    exit(1)
 
 def tela_inicial():
+    global n_cidades
+    global prob_ataque
+    global max_dist
     opc=input("1-Iniciar\n2-Opções\n\n0-Sair\n\n\n: ")
     if opc == "1":
-        start()
+        start(n_cidades,prob_ataque,max_dist)
     elif opc == "2":
-        config()
+        n_cidades,prob_ataque,max_dist=config(n_cidades,prob_ataque,max_dist)
     elif opc == "0":
         sair()
     else:
         clear()
-        tela_inicial()
+    tela_inicial()
 
 
 
