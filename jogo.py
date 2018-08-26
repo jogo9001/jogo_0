274ae24e4daa2036e0e514d8ed52314e46874fae
zSucrilhos <e.cesar88@gmail.com>
2018-08-26 00:50:51 -0300

    adads

--- jogo.py
+++ jogo.py
@@ -2,14 +2,8 @@
 # -*- coding: utf-8 -*-
 import os
 def inicio():
-    if os.name == "posix":
-        os.system('clear')
-        print('Estou num POSIX')
-    elif os.name in ("nt", "dos", "ce"):
-        os.system('cls')
-        print('Estou num win')
-    else:
-        print('Não estou num unix nem num win')
+    clear()
+    print(os.name)
 
 def clear():
     if os.name == "posix":
