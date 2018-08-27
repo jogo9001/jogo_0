53e3b200b8eae9e2c8d7f3019da0df57f1aafedf
zSucrilhos <e.cesar88@gmail.com>
2018-08-27 00:45:47 -0300

    funcoes ok

--- mat.py
+++ mat.py
@@ -11,3 +11,17 @@ def testa_ataque(intervalo):
     else:
         return False
 
+def raiz_quadrada(n):
+    a = n**0.5
+    return a
+
+def calc_dist(px1, py1, px2, py2):
+    calc1 = (px1-px2)*(px1-px2)
+    calc2 = (py1-py2)*(py1-py2)
+    calc3 = calc1 + calc2
+    calc4 = raiz_quadrada(calc3)
+    return float(calc4)
+
+if __name__ == "__main__":
+    calc_dist(3,1,5,2)
+
