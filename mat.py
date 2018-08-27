d05aeda143edfa202447b1bf895f3980fd498619
zSucrilhos <e.cesar88@gmail.com>
2018-08-26 23:01:25 -0300

    mat.py v2

--- mat.py
+++ mat.py
@@ -4,3 +4,10 @@ def n_aleatorio(a,b):
     n = randint(a,b)
     return n
 
+def testa_ataque(intervalo):
+    n = n_aleatorio(1,intervalo)
+    if n == 1:
+        return True
+    else:
+        return False
+
