b299cb43b7a8c7361b54873aa368dd3085defff2
asadefrango <andre.santana.de.araujo@gmail.com>
2018-09-04 13:37:07 -0400

    add gencoord

--- mat.py
+++ mat.py
@@ -21,13 +21,12 @@ def calc_dist(px1, py1, px2, py2):
     calc3 = calc1 + calc2
     calc4 = raiz_quadrada(calc3)
     return calc4
-
+def gen_cidade_coordenadas(screen_min_x, screen_max_x, screen_min_y, screen_max_y):
+    x=n_aleatorio(screen_min_x,screen_max_x)
+    y=n_aleatorio(screen_min_y,screen_max_y)
+    return [x,y]
 def checa_dist(px1, py1, px2, py2, MAXDIST):
     if calc_dist(px1, py1, px2, py2) > MAXDIST and calc_dist(px1, py1, px2, py2) == 0:
         return False
     else:
         return True
-
-if __name__ == "__main__":
-    calc_dist(3,1,5,2)
-
